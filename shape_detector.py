ec4edab0d5ff8cd1e0fbee9101ed729dbe8ce770
highshot-rk <ivannosal22@gmail.com>
2021-05-18 08:51:07 +0900

    init

---
+++ shape_detector.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 03:07:09 2021
+
+@author: Lucy
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import sys
+
+# read the image from arguments
+image = cv2.imread(sys.argv[1])
+
+# convert to grayscale
+grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# perform edge detection
+edges = cv2.Canny(grayscale, 30, 100)
+
+# detect lines in the image using hough lines technique
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, 60, np.array([]), 50, 5)
+# iterate over the output lines and draw them
+for line in lines:
+    for x1, y1, x2, y2 in line:
+        cv2.line(image, (x1, y1), (x2, y2), color=(20, 220, 20), thickness=3)
+
+# show the image
+plt.imshow(image)
+plt.show()
---
+++ start.bat
@@ -0,0 +1,2 @@
+pip install opencv-python
+python live_shape_detector.py
